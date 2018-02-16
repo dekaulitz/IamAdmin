557b132dc6c93958d2cc84c504311a3f41282b91
fahmi <fahmi@code.id>
2018-02-16 17:25:29 +0700

    update webpack update scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,98 @@
 import 'jquery'
-import '../css/main.scss'
-import '../users.html'
+import 'chart.js'
+import 'popper.js'
+import 'bootstrap'
+//import 'font-awesome/scss/font-awesome.scss';
+import '../scss/stylesheet.scss'
+import '../dashboard.html'
 import {RandomGenerator} from "./random-generator";
 
-const outputParagraph = $("#outputParagraph");
-const outputRandomInt = () => {
-    outputParagraph.text(RandomGenerator.randomInteger());
+// const outputParagraph = $("#outputParagraph");
+// const outputRandomInt = () => {
+//     outputParagraph.text(RandomGenerator.randomInteger());
+// };
+// const outputRandomRange = function () {
+//     outputParagraph.text(RandomGenerator.randomRange(1, 1000));
+// };
+// const buttonRandInt = jQuery("#randomInt");
+// const buttonRandRange = document.querySelector("#randomRange");
+//
+// buttonRandInt.click(outputRandomInt);
+// buttonRandRange.addEventListener('click', outputRandomRange);
+var color = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
 };
-const outputRandomRange = function () {
-    outputParagraph.text(RandomGenerator.randomRange(1, 1000));
+var pieChartData = {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [
+                RandomGenerator.randomInteger(),
+                RandomGenerator.randomInteger(),
+                RandomGenerator.randomInteger(),
+                RandomGenerator.randomInteger(),
+                RandomGenerator.randomInteger(),
+            ],
+            backgroundColor: [
+                color.red,
+                color.orange,
+                color.yellow,
+                color.green,
+                color.blue,
+            ],
+            label: 'Dataset 1'
+        }],
+        labels: [
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue"
+        ]
+    },
+    options: {
+        responsive: true
+    }
 };
-const buttonRandInt = jQuery("#randomInt");
-const buttonRandRange = document.querySelector("#randomRange");
+var pieChart = document.getElementById("pieChart").getContext("2d");
+window.myPie = new Chart(pieChart, pieChartData);
 
-buttonRandInt.click(outputRandomInt);
-buttonRandRange.addEventListener('click', outputRandomRange);
+
+var barChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: 'Dataset 1',
+            backgroundColor: color.red,
+            data: [RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger()]
+        },
+        {
+            label: 'Dataset 2',
+            backgroundColor: color.blue,
+            data: [RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger(), RandomGenerator.randomInteger()]
+        }
+    ]
+};
+
+var ctx = document.getElementById("barChart").getContext("2d");
+window.myBar = new Chart(ctx, {
+    responsive: true,
+    type: 'bar',
+    data: barChartData,
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Bar Chart'
+        }
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,14 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var extractPlugin = new ExtractTextPlugin({
-    filename: './css/main.css',
+    filename: './css/stylesheet.css',
 });
 
 module.exports = {
+
     entry: {
         app_js: './src/js/app.js',
+        //  app_css:'./src/css/stylesheet.scss'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -19,21 +21,28 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ['es2015']
-                        }
-                    }
-                ]
+                test: /\.js$/, use: [{loader: "babel-loader", options: {presets: ['es2015']}}]
             },
             {
-                test: /\.scss$/,
-                use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
+                test: /\.s[ac]ss$/, loader: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'resolve-url-loader', 'sass-loader'],
+                    fallback: 'style-loader'
                 })
+            },
+            {
+                test: /\.sass$/, loader: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                })
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',    // where the fonts will go
+                        publicPath: '../'       // override the default path
+                    }
+                }]
             },
             {
                 test: /\.html$/,
@@ -44,8 +53,7 @@ module.exports = {
                     }
                 }],
 
-            },
-            {
+            }, {
                 test: /\.(jpg|png)/,
                 use: [{
                     loader: "file-loader",
@@ -56,33 +64,35 @@ module.exports = {
                     }
                 }]
             },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: '[name].[ext]',
-                    }
-                }],
-                exclude: path.resolve(__dirname, 'src/index.html')
-            }
+            // {
+            //     test: /\.html$/,
+            //     use: [{
+            //         loader: "file-loader",
+            //         options: {
+            //             name: '[name].[ext]',
+            //         }
+            //     }],
+            //     exclude: path.resolve(__dirname, 'src/index.html'),
+            //
+            // }
         ]
     },
     plugins: [
         new Webpack.ProvidePlugin({
             $: 'jquery',
-            jQuery: 'jquery'
+            jQuery: 'jquery',
+            Popper: 'pooper.js',
+            chartjs: 'chart.js'
         }),
         extractPlugin,
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/index.html'
         }),
-        // new HtmlWebpackPlugin({
-        //     filename: 'users.html',
-        //     template: 'src/users.html',
-        //     chunks: ['app_js']
-        // }),
+        new HtmlWebpackPlugin({
+            filename: 'dashboard.html',
+            template: 'src/dashboard.html',
+        }),
         new CleanWebpackPlugin(['dist'])
     ]
 };
