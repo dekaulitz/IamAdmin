9b87f8e81d4bcf6f9d46cf81e3b2c7170001984b
fahmi <fahmi@code.id>
2018-02-20 00:19:13 +0700

    remove app.js

--- src/js/common.js
+++ src/js/common.js
@@ -5,5 +5,3 @@ import 'bootstrap'
 import 'bootstrap-datepicker';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.standalone.css'
 import '../scss/stylesheet.scss'
-
-var app = require('./app');
